f2cf78dc1dd69e0aa2d815dbf7b11ec001722281
Denis Sergienko <sergden.01@gmail.com>
2021-02-15 17:09:58 +0300

    teclast - add models

---
+++ OOOFormula/Models/Contacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOOFormula.Models
+{
+    public class Contacts
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ OOOFormula/Models/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOOFormula.Models
+{
+    public class Products
+    {
+        public int Id{ get; set; }
+
+        public string Name{ get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Description { get; set; }
+
+        public IEnumerable<string> ImagesName { get; set; }
+
+        public int ManufacturerId { get; set; }
+
+        public int MaterialId { get; set; }
+
+    }
+}
