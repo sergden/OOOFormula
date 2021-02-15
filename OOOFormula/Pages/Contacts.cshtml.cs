94215a2eb5234096d432275914d157ab9ec1be88
Denis Sergienko <sergden.01@gmail.com>
2021-02-15 16:42:41 +0300

    teclast - add page 'Contacts'

---
+++ OOOFormula/Pages/Contacts.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OOOFormula.Pages
+{
+    public class ContactsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            TempData["SuccessMessage"] = "Сообщение отправлено";
+        }
+    }
+}
