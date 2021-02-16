c333fefa9d7fbe8e59eb90b0f01ceb3a139f6c7b
Denis Sergienko <sergden.01@gmail.com>
2021-02-16 13:10:34 +0300

    teclast

--- OOOFormula/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ OOOFormula/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,7 +73,7 @@ namespace OOOFormula.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Identity/Account/Manage");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         
