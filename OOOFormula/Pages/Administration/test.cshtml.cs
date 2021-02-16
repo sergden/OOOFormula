ee81132ba9605cb2d4bddb3f69afb074c7644008
Denis Sergienko <sergden.01@gmail.com>
2021-02-16 18:50:57 +0300

    teclast - add test page

---
+++ OOOFormula/Pages/Administration/test.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OOOFormula.Data;
+using OOOFormula.Models;
+
+namespace OOOFormula.Pages.Administration
+{
+    public class testModel : PageModel
+    {
+        private readonly OOOFormula.Data.ApplicationDbContext _context;
+
+        public testModel(OOOFormula.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public OurServices OurServices { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            OurServices = await _context.OurServices.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (OurServices == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
