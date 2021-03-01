7f27401cb599a165ec50c3941ec058532ecf3ba5
Denis Sergienko <sergden.01@gmail.com>
2021-03-01 16:34:46 +0300

    add private method

--- OOOFormula/Pages/Catalog/Catalog.cshtml.cs
+++ OOOFormula/Pages/Catalog/Catalog.cshtml.cs
@@ -25,31 +25,41 @@ namespace OOOFormula.Pages.Catalog
         public IEnumerable<Materials> Materials { get; set; }
 
         //For save state sorting
-        [BindProperty(SupportsGet = true)]
+        [BindProperty]
         public SortState? PriceState { get; set; }
 
         [BindProperty(SupportsGet = true)]
         public int? MaterialIdState { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(decimal PriceFrom, decimal PriceTo, SortState? sortOrder, int? MaterialId_select, string searchString)
+        public async Task<IActionResult> OnGetAsync(decimal PriceFrom, decimal PriceTo, SortState? sortPrice, int? MaterialId_select, string searchString)
         {
             Products = await _context.Products.Where(p => p.status == true).AsNoTracking().ToListAsync(); //извлекаем из БД все записи
             ViewData["MaterialsId"] = new SelectList(_context.Materials, "Id", "Name"); //получаем материалы
 
             //сохраняем состояние фильтрации
-            if (sortOrder != null) PriceState = sortOrder;
+            if (sortPrice != null) PriceState = sortPrice;
             if (MaterialId_select != null) MaterialIdState = MaterialId_select;
 
-            //поиск, если есть строка поиска
-            if (!string.IsNullOrWhiteSpace(searchString))
-            {
-                Products = Products.Where(p =>
-                p.Name.ToLower().Contains(searchString.ToLower()) ||
-                p.Description.ToLower().Contains(searchString.ToLower())
-                );
-            }
+            //Поиск
+            Search(searchString);
 
             //обрабатываем по фильтрам
+            SortingPrice(PriceFrom, PriceTo);
+
+            if (!Products.Any()) //проверяем, есть ли что-то
+            {
+                TempData["Message"] = "Ничего не найдено";
+                return Page();
+            }
+
+            //сортировка
+            Sorting();
+
+            return Page();
+        }
+
+        private void SortingPrice(decimal PriceFrom, decimal PriceTo)
+        {
             if (PriceFrom >= 0 && PriceTo > 0)
             {
                 Products = Products.Where(x => x.Price >= PriceFrom && x.Price <= PriceTo);
@@ -63,22 +73,28 @@ namespace OOOFormula.Pages.Catalog
             {
                 Products = Products.Where(x => x.MaterialsId == MaterialIdState);
             }
+        }
 
-            if (!Products.Any()) //проверяем, есть ли что-то
-            {
-                TempData["Message"] = "Ничего не найдено";
-                return Page();
-            }
-
-            //сортировка
+        private void Sorting()
+        {
             Products = PriceState switch
             {
                 SortState.PriceAsc => Products.OrderBy(p => p.Price),
                 SortState.PriceDesc => Products.OrderByDescending(p => p.Price),
                 _ => Products.OrderBy(p => p.Id),
             };
+        }
 
-            return Page();
+        private void Search(string searchString)
+        {
+            //поиск, если есть строка поиска
+            if (!string.IsNullOrWhiteSpace(searchString))
+            {
+                Products = Products.Where(p =>
+                p.Name.ToLower().Contains(searchString.ToLower()) ||
+                p.Description.ToLower().Contains(searchString.ToLower())
+                );
+            }
         }
 
         public async Task<IActionResult> OnGetResetAsync()
