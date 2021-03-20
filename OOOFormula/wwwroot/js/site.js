24231e301af0042605874fe028a84bdb51d7cb6d
Denis Sergienko <sergden.01@gmail.com>
2021-03-20 10:52:56 +0300

    add view-password-button

--- OOOFormula/wwwroot/js/site.js
+++ OOOFormula/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('body').on('click', '.password-control', function () {
+    if ($('#password-input').attr('type') == 'password') {
+        $(this).addClass('view');
+        $('#password-input').attr('type', 'text');
+    }
+    else {
+        $(this).removeClass('view');
+        $('#password-input').attr('type', 'password');
+    }
+    return false;
+});
+
+$('body').on('click', '.password-control-new', function () {
+    if ($('#password-input-new').attr('type') == 'password') {
+        $(this).addClass('view');
+        $('#password-input-new').attr('type', 'text');
+    }
+    else {
+        $(this).removeClass('view');
+        $('#password-input-new').attr('type', 'password');
+    }
+    return false;
+});
+
+$('body').on('click', '.password-control-confirm', function () {
+    if ($('#password-input-confirm').attr('type') == 'password') {
+        $(this).addClass('view');
+        $('#password-input-confirm').attr('type', 'text');
+    }
+    else {
+        $(this).removeClass('view');
+        $('#password-input-confirm').attr('type', 'password');
+    }
+    return false;
+});
